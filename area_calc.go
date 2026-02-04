bced2c0591740d43949d28639f66cbf6317c0a78
Minato <minatoshadow16@gmail.com>
2026-02-04 16:39:12 +0530

    Add files via upload
    You need a main file to run it and it's my first project

---
+++ area_calc.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func AreaCalculator() {
+		fmt.Println("Welcome To Area Calculator Of Rectangle") // Welcome user
+	// Initialising variables
+	var length, breadth float64 
+	var unit string
+	
+	fmt.Print("Enter Length: "); fmt.Scan(&length) // Asking for length
+	fmt.Print("Enter Breadth: "); fmt.Scan(&breadth) // Asking for breadth
+	fmt.Print("Enter Unit: "); fmt.Scan(&unit) // Asking for unit
+
+	area := length * breadth // Calculate Area
+	
+	fmt.Printf("Area of Rectangle is %v%v²\n", area, unit) // Print area
+}
